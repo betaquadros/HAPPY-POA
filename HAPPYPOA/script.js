0832149ea9e148c5932216f84af5a0a55d534041
Roberta Quadros <betaquadros7@gmail.com>
2026-06-17 09:11:26 -0300

    Add files via upload

---
+++ HAPPYPOA/script.js
@@ -0,0 +1,14 @@
+```javascript
+console.log("Sistema carregado ✔");
+```
+function aumentarFonte() {
+    document.body.style.fontSize = "130%";
+}
+
+function diminuirFonte() {
+    document.body.style.fontSize = "100%";
+}
+
+function toggleModoCalmo(){
+    document.body.classList.toggle("modo-calmo");
+}
